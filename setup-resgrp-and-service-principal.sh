b658704c04f8bf3a9548d967dc324f18505f7210
daumsimac <daumsimac@gmail.com>
2021-06-17 15:39:00 +0900

    Update setup-resgrp-and-service-principal.sh

--- setup-resgrp-and-service-principal.sh
+++ setup-resgrp-and-service-principal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create Resource group
-az group create -n vmss-blue-green-rg -l southeastasia
+az group create -n vmss-blue-green-rg -l koreacentral
 
 # Create Service principal which will be used by packer
 az ad sp create-for-rbac -n "azdevops-vmms-deploy" 
